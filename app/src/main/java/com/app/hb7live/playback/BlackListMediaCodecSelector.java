7d5caa5ff52a6a635cce267205d1b808f0828de8
issam <issamelguerch@gmail.com>
2022-03-09 00:57:06 +0100

    update 4

---
+++ app/src/main/java/com/app/hb7live/playback/BlackListMediaCodecSelector.java
@@ -0,0 +1,47 @@
+package com.app.hb7live.playback;
+
+import com.google.android.exoplayer2.mediacodec.MediaCodecInfo;
+import com.google.android.exoplayer2.mediacodec.MediaCodecSelector;
+import com.google.android.exoplayer2.mediacodec.MediaCodecUtil;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import androidx.annotation.Nullable;
+
+public class BlackListMediaCodecSelector implements MediaCodecSelector {
+
+    // list of strings used in blacklisting codecs
+    //final static String[] BLACKLISTEDCODECS = {};
+
+    final static String[] BLACKLISTEDCODECS = {"OMX.google.h264.decoder"};
+
+    @Override
+    public List<MediaCodecInfo> getDecoderInfos(String mimeType, boolean requiresSecureDecoder, boolean requiresTunnelingDecoder) throws MediaCodecUtil.DecoderQueryException {
+
+        List<MediaCodecInfo> codecInfos = MediaCodecUtil.getDecoderInfos(
+                mimeType, requiresSecureDecoder, requiresTunnelingDecoder);
+        // filter codecs based on blacklist template
+        List<MediaCodecInfo> filteredCodecInfos = new ArrayList<>();
+        for (MediaCodecInfo codecInfo: codecInfos) {
+            boolean blacklisted = false;
+            for (String blackListedCodec: BLACKLISTEDCODECS) {
+                System.out.println("codecInfo.name : " +codecInfo.name);
+                if (codecInfo.name.contains(blackListedCodec)) {
+                    blacklisted = true;
+                    break;
+                }
+            }
+            if (!blacklisted) {
+                filteredCodecInfos.add(codecInfo);
+            }
+        }
+        return filteredCodecInfos;
+    }
+
+    @Nullable
+    @Override
+    public MediaCodecInfo getPassthroughDecoderInfo() throws MediaCodecUtil.DecoderQueryException {
+        return MediaCodecUtil.getPassthroughDecoderInfo();
+    }
+}
